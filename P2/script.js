09ac8ee1fd9c450b26eff70e7b7b2dad00b987b4
mrojbar <miguel24rb@gmail.com>
2020-02-20 22:59:44 +0100

    commit

--- P2/script.js
+++ P2/script.js
@@ -1,29 +1,22 @@
-//-- Leer el párrafo identificado como test
 const pantalla = document.getElementById('pantalla')
+const botonSuma = document.getElementById('botonSuma')
 const boton1 = document.getElementById('boton1')
 const boton2 = document.getElementById('boton2')
-//-- Mostrar en la consola el contenido del párrafo
-//-- (es la propiedad innerHTML)
 
-//-- Configurar el manejador para el evento de
-//-- pulsación de botón
-//vars
-clicked1 = false;
-
+//funcion de escritura de valor en pantalla
+function escribeBoton (objeto){
+  if (pantalla.innerHTML == 0)
+    pantalla.innerHTML = objeto;
+  else {
+  add = document.createTextNode(objeto);
+  document.getElementById("pantalla").appendChild(add);
+  }
+}
+//limpiar pantalla CLEAR
 botonclear.onclick = () => {
     pantalla.innerHTML = 0;
 }
-
-boton1.onclick = () => {
-  add = document.createElement("P");
-  num = 1;
-  add.innerHTML = num.toString(10);
-  document.getElementById("pantalla").appendChild(add);
-}
-
-boton2.onclick = () => {
-  add = document.createElement("P");
-  num = 2;
-  add.innerHTML = num.toString(10);
-  document.getElementById("pantalla").appendChild(add);
-}
+//eventos de botones
+botonSuma.onclick = function () {escribeBoton("+")};
+boton1.onclick = function () {escribeBoton(1)};
+boton2.onclick = function () {escribeBoton(2)};
