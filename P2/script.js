5b0552fbe5ed3237c4873d23023527d628a47ef9
Miguel Rojas Barbero <mrojasb@f-l3210-pc17.aulas.etsit.urjc.es>
2020-02-20 10:53:50 +0100

    calculadora

--- P2/script.js
+++ P2/script.js
@@ -1,13 +1,29 @@
 //-- Leer el párrafo identificado como test
-const test = document.getElementById('test')
-const test = document.getElementById('test2')
+const pantalla = document.getElementById('pantalla')
+const boton1 = document.getElementById('boton1')
+const boton2 = document.getElementById('boton2')
 //-- Mostrar en la consola el contenido del párrafo
 //-- (es la propiedad innerHTML)
 
-console.log("Párrafo test leido. Dice:")
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón
+//vars
+clicked1 = false;
 
-console.log(test.innerHTML)
+botonclear.onclick = () => {
+    pantalla.innerHTML = 0;
+}
 
-test.onclick = () => {
-  console.log("Click sobre el párrafo...")
+boton1.onclick = () => {
+  add = document.createElement("P");
+  num = 1;
+  add.innerHTML = num.toString(10);
+  document.getElementById("pantalla").appendChild(add);
+}
+
+boton2.onclick = () => {
+  add = document.createElement("P");
+  num = 2;
+  add.innerHTML = num.toString(10);
+  document.getElementById("pantalla").appendChild(add);
 }
