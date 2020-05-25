5fed7f0270678e480b70a29a1ee20480a57efa7d
mrojbar <miguel24rb@gmail.com>
2020-05-25 14:46:49 +0200

    modos

--- P4/video.js
+++ P4/video.js
@@ -1,6 +1,4 @@
-console.log("Ejecutando JS...");
-
-//----- Obtener elemento de video y configurarlo
+//----- Videos
 var video1 = document.getElementById("video1")
 video1.width=427;  //-- Tamaño de la pantalla de video
 video1.height=240;
@@ -23,55 +21,136 @@ video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
 video4.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
 video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
 
+//-- var global de modo
+var FlagModoNormal = true; //true normal, false auto.
 
-//-- Obtener los botones
+//-- Botones
+const ModoNormal = document.getElementById("modonormal");
+const ModoAuto = document.getElementById("modoauto");
 const play1 = document.getElementById("play1");
 const play2 = document.getElementById("play2");
 const play3 = document.getElementById("play3");
 
-//-- Función de retrollamada del botón de ver
-play1.onclick = () => {
-  console.log("Play video 1");
-  document.getElementById("video2").style.border = "thick solid rgba(255,51,51,1)";
-  document.getElementById("video3").style.border = "thick solid rgba(255,51,51,0)";
-  document.getElementById("video4").style.border = "thick solid rgba(255,51,51,0)";
-  video1.src = video2.src;
-  video1.currentTime = video2.currentTime;
-  video1.play();
-};
+//-- Funciones
 
-play2.onclick = () => {
-  console.log("Play video 1");
-  video1.src = video3.src;
-  document.getElementById("video2").style.border = "thick solid rgba(255,51,51,0)";
-  document.getElementById("video3").style.border = "thick solid rgba(255,51,51,1)";
-  document.getElementById("video4").style.border = "thick solid rgba(255,51,51,0)";
-  video1.currentTime = video3.currentTime;
-  video1.play();
-};
-
-play3.onclick = () => {
-  console.log("Play video 1");
-  document.getElementById("video2").style.border = "thick solid rgba(255,51,51,0)";
-  document.getElementById("video3").style.border = "thick solid rgba(255,51,51,0)";
-  document.getElementById("video4").style.border = "thick solid rgba(255,51,51,1)";
-  video1.src = video4.src;
-  video1.currentTime = video4.currentTime;
-  video1.play();
-};
-
-//-- Funcion de retrollamada del boton de parar
-stop.onclick = () => {
-  video1.pause();
-  //-- Quitar la fuente de video, para que se muestre la
-  //-- imagen definida en el atributo poster
-  video1.src=null;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function inicio(){
+function coloreaMarcoFuente(num){
+  if (num == 0){
+    document.getElementById("video2").style.border = "thick solid rgba(255,51,51,0)";
+    document.getElementById("video3").style.border = "thick solid rgba(255,51,51,0)";
+    document.getElementById("video4").style.border = "thick solid rgba(255,51,51,0)";
+  }
+  else if (num == 1){
+    document.getElementById("video2").style.border = "thick solid rgba(255,51,51,1)";
+    document.getElementById("video3").style.border = "thick solid rgba(255,51,51,0)";
+    document.getElementById("video4").style.border = "thick solid rgba(255,51,51,0)";
+  }
+  else if (num == 2){
+    document.getElementById("video2").style.border = "thick solid rgba(255,51,51,0)";
+    document.getElementById("video3").style.border = "thick solid rgba(255,51,51,1)";
+    document.getElementById("video4").style.border = "thick solid rgba(255,51,51,0)";
+  }
+  else if (num == 3){
+    document.getElementById("video2").style.border = "thick solid rgba(255,51,51,0)";
+    document.getElementById("video3").style.border = "thick solid rgba(255,51,51,0)";
+    document.getElementById("video4").style.border = "thick solid rgba(255,51,51,1)";
+  }
+}
+
+ModoNormal.onclick = () => {
+  document.getElementById("modonormal").style.border = "thick solid rgba(255,51,51,1)";
+  document.getElementById("modonormal").style.color = "rgba(255,51,51,1)";
+  document.getElementById("modoauto").style.border = "thick solid rgba(255,51,51,0)";
+  document.getElementById("modoauto").style.color = "white";
+  FlagModoNormal = true;
+  normal();
+}
+
+ModoAuto.onclick = () => {
+  document.getElementById("modoauto").style.border = "thick solid rgba(255,51,51,1)";
+  document.getElementById("modoauto").style.color = "rgba(255,51,51,1)";
+  document.getElementById("modonormal").style.border = "thick solid rgba(255,51,51,0)";
+  document.getElementById("modonormal").style.color = "white";
+  FlagModoNormal = false;
+  auto();
+}
+
+async function auto(){
+
   video2.play();
   video3.play();
   video4.play();
+
+  coloreaMarcoFuente(0);
+
+  while(FlagModoNormal == false){
+
+    if(FlagModoNormal == true) break;
+
+    video1.src = video2.src;
+    video1.currentTime = video2.currentTime;
+    coloreaMarcoFuente(1);
+    video1.play();
+
+    await sleep(3000);
+
+    if(FlagModoNormal == true) break;
+
+    video1.src = video3.src;
+    video1.currentTime = video3.currentTime;
+    coloreaMarcoFuente(2);
+    video1.play();
+
+    await sleep(3000);
+
+    if(FlagModoNormal == true) break;
+
+    video1.src = video4.src;
+    video1.currentTime = video4.currentTime;
+    coloreaMarcoFuente(3);
+    video1.play();
+
+    await sleep(3000);
+  }
 }
 
-inicio();
+function normal(){
+  video2.play();
+  video3.play();
+  video4.play();
+
+  play1.onclick = () => {
+    if (FlagModoNormal == true){
+      console.log("Play video 1");
+      coloreaMarcoFuente(1);
+      video1.src = video2.src;
+      video1.currentTime = video2.currentTime;
+      video1.play();
+    }
+  };
+
+  play2.onclick = () => {
+    if (FlagModoNormal == true){
+      console.log("Play video 1");
+      video1.src = video3.src;
+      coloreaMarcoFuente(2);
+      video1.currentTime = video3.currentTime;
+      video1.play();
+    }
+  };
+
+  play3.onclick = () => {
+    if (FlagModoNormal == true){
+      console.log("Play video 1");
+      coloreaMarcoFuente(3);
+      video1.src = video4.src;
+      video1.currentTime = video4.currentTime;
+      video1.play();
+    }
+  };
+}
+
+normal();
